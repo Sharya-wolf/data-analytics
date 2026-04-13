fe3db277a0e7577ea58a0bb502723a5c4ed8f15d
Sha'Rya Weaver <sharyaweaver3@gmail.com>
2026-04-13 15:27:08 -0400

    Exercise 1.C complete

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,10 @@
+/*
+a) The information included in the table actor and then columns is the information on the actor. 
+b) The information included in the table film and then columns is the complete information and description of the film. 
+d) The information included in the table for rental has all the dates for the rental from the date it was purchased to the date it was returned as well as the Id's of the rental, customer, staff and inventory. Lastly, it includes the last update on the rental. 
+e) The information included in the table for inventory has the Id's for all inventory, film, and store. It also has the last update. 
+f) The tables you need is the film table and rental table. These table are related to eachother because they both speak about the films that was rented and both give information on the films. The rental table provides the information of those that are rented while the film table is the one that would provide the names and better description of the film. 
+
+SELECT rental_date FROM rental
+SELECT title FROM film
+*/

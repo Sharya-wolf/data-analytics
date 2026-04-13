dfd2a056ee08933f35a8803adcc18d9ef3d69f5c
Sha'Rya Weaver <sharyaweaver3@gmail.com>
2026-04-13 12:40:31 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about sales and progress for use by analyst. 
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [200] records
+SELECT * FROM film; -- Retrieved [1000] records  

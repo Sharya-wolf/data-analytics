5298f68f582f1bae3a57f42af0d87c35b6875339
Sha'Rya Weaver <sharyaweaver3@gmail.com>
2026-04-24 12:33:07 -0400

    add exercise 2A

---
+++ week-03/2A_using_joins.sql
@@ -0,0 +1,63 @@
+USE northwind;
+-- 1. Create a single query to list the product id, product name, unit price and category name of all products. 
+-- Order by category name and within that, by product name. --
+
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName
+FROM products p
+INNER JOIN categories c
+       ON c.CategoryID = p.CategoryID
+ORDER BY c.CategoryName, p.Productname;
+
+
+-- 2. Create a single query to list the product id, product name, unit price and supplier name of all products that cost more than $75. 
+-- Order by product name.--
+
+SELECT p.ProductID, p.ProductName, p.UnitPrice, s.CompanyName
+FROM products p
+INNER JOIN suppliers s
+		ON p.SupplierID = s.SupplierID
+WHERE p.UnitPrice > 75 ORDER BY p.ProductName;
+
+
+-- 3. Create a single query to list the product id, product name, unit price, category name, and supplier name of every product. 
+-- Order by product name.
+
+SELECT p.ProductID, p.ProductName, p.UnitPrice, c.CategoryName, s.CompanyName
+FROM products p 
+INNER JOIN categories c 
+		ON p.CategoryID = c.CategoryID 
+INNER JOIN supplier s
+        ON p.SupplierID = s.SupplierID
+ORDER BY p.ProductName;
+
+
+-- 4. Create a single query to list the order id, ship name, ship address, and shipping company name of every order that shipped to Germany. 
+-- Assign the shipping company name the alias ‘Shipper.’ Order by the name of the shipper, then the name of who it shipped to.
+
+SELECT o.OrderID, o.ShipName, o.ShipAddress, o.ShipCountry, s.CompanyName AS 'Shipper'
+FROM orders o
+INNER JOIN shippers s
+        ON o.Shipvia = s.ShipperID
+WHERE o.ShipCountry = 'Germany' 
+ORDER BY Shipper, ShipAddress;
+
+-- 5. Start from the same query as above (#4), but omit OrderID and add logic to group by ship name, with a count of how many orders were shipped for that ship name.
+
+SELECT COUNT(*) AS 'OrderCount', o.ShipName, s.CompanyName AS 'Shipper'
+FROM orders o
+INNER JOIN shippers s
+        ON o.Shipvia = s.ShipperID
+WHERE o.ShipCountry = 'Germany'
+GROUP BY o.ShipName, s.CompanyName 
+ORDER BY s.CompanyName, o.ShipName;
+
+-- 6. Create a single query to list the order id, order date, ship name, ship address of all orders that included Sasquatch Ale.
+
+SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
+FROM orders o
+INNER JOIN `order details` od
+        ON o.OrderID = od.OrderID
+INNER JOIN products p
+        ON p.ProductID = od.ProductID
+WHERE p.ProductName LIKE '%Sasquatch Ale%' 
+ORDER BY o.OrderID;

79e6441dfda36f40aa51d63a721c060ba91a232b
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-04 15:11:41 -0400

    demo1 practice

---
+++ week-05/demo1.py
@@ -0,0 +1,18 @@
+# Sha'Rya Weaver
+# 5/4/2026
+
+name = "Marty"
+age = 20
+
+print(name)
+print(age)
+
+print(f'My name is {name} and I am {age} years old.')
+
+pi = 3.145678
+
+print(f"The value of pi is {pi:.2f}")
+
+sale = 12345.7685
+
+print(f"The value of pi is {sale:,.2f}")

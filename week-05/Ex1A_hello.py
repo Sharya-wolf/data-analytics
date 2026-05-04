2ef3e92d38eb32cf6f56707903c7d6cb3d45f050
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-04 15:54:50 -0400

    add exercise 1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print ('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world" prints twice because both does and means the same thing.
+
+# Displaying dollars and cents
+
+dollars =  3
+cents = 50
+print(dollars + cents)
+# The results give the what the value of the variable equals and does the equation as if it was a simple equation
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)

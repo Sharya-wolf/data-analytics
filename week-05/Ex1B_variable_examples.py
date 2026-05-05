473b8998025fffb2a6b6365966cdf054b375b69f
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-05 08:52:55 -0400

    Completed 1B exercise

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,23 @@
+customer_id = 512
+Customers_name = 'Linda_Barnes'
+Customer_gender = 'Female'
+Customer_dateofbirth = '5/6/2000'
+Drivers_license_number = 45567899
+Auto_policynumber = 5445
+ 
+ # I wrote the first and last name yet because there is a space I subbed it for a underscore
+ # Most of the spaces was subbed using an underscore
+# The date of birth one has to have single quotations because of the slashes in between the numbers
+
+
+my_name = "Sha'Rya" 
+born_raised = 'Harlem'
+
+# False, def, if, raise, None, del, import, return, True, elif, in, try, and, else, is, while, as, except, lambda, with,
+# assert, finally, nonlocal, yield, break, for, not, class, form, or, continue, global, pass are all keywords.
+
+# def- Defines a function or method in Python
+# raise- Used to raise an error those errors are then raised in the tracebook and cancel execution.
+# return- Used to exit a function or a method and return some value.
+# elif- Shortened version to write if not, checks if some other condition holds when the condition if statement is false.
+# nonlocal- Used in functions inside functions to create anonymous functions.

ccf4e76f5b8e09c62eee67f58f639d20616ecd74
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-05 14:54:35 -0400

    Logical and Relational Operators

---
+++ week-05/demo2.py
@@ -0,0 +1,56 @@
+# Sha'Rya Weaver
+# 5/5/2026
+
+# Logical Operators
+x = 10
+y = 5
+
+result = (x > 5) and (y < 10)
+print (result) # True
+
+# Two
+
+a = 3
+b = 8
+result = (x > 5) or (b > 5)
+print (result) # True
+
+# Three
+
+x = 10
+y = 5
+result = not (x > y)
+print (result) # False
+
+
+# Relational operators
+
+# 1. Equal to ==
+a = 5
+b = 5
+print (a == b) # True
+
+# 2. Not Equal to !=
+x = 10
+y = 3
+print (x != y) # True
+
+# 3. Greater than >
+num1 = 7
+num2 = 4
+print (num1 > num2) #True
+
+# 4. Less than <
+a = 2
+b = 8
+print (a < b) # True
+
+# 5. Greater than  or equal to >=
+x = 6
+y = 6
+print (x >= y) # True
+
+# 6. Less than or equal to <=
+p = 3
+q = 5
+print (p <= q) # True

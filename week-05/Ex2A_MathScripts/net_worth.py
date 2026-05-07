88dfd319851c45b8a77593943098fa5d5aea69e9
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-07 16:50:04 -0400

    Completed first math script

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,24 @@
+# Sha'Rya Weaver
+# 5/5/2026
+
+# How do you calculate your net worth given your assets and debts?
+
+# a) Start by brainstorming: What are "assets" that would need to be included in this calculation? What about "debts"?
+    # Your assests is like your income or just how much you have as of now in your bank account, savings, etc.
+    # Your debts is everything you owe or is currently paying off.
+
+# b) Discuss and fugure out the formula and what the script would look like, making up example values as needed.
+        # The formula to find your total net worth is your total assets minus your total debts.
+
+# c) Now create the script.
+
+# Script:
+Assets = 92.59 + 88.98
+print(f"Your total assests are {Assets}")
+Debts = 373.64 + 1701.84
+print(f"Your debts are {Debts}")
+Net_worth = Assets - Debts
+print(f"Your net worth is {Net_worth}")
+
+
+

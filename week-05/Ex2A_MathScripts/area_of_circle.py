fe1df3111dfa13df60287e8b4909be84ca0d69e8
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-08 11:20:47 -0400

    Completed area of circle

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,21 @@
+# Sha'Rya Weaver
+# 5/7/2026
+
+# How do you calculate the area of a circle?
+     # a) The diameter of a given circle is the same as the day of your birthday
+     # b) Create the script
+
+# Script
+diameter = 23
+radius = diameter / 2
+
+# Calculations
+from math import pi
+
+area = pi * (radius ** 2)
+
+# Results
+print(f"The area of a circle with radius {radius} is {area:.2f}")
+
+# Output: The area of a circle with radius 11.5 is 415.48.
+

77e54109eede7a77b53c23fefc69df0974482676
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-08 10:59:47 -0400

    Completed tip amount

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,18 @@
+# Sha'Rya Weaver
+# 5/7/2026
+
+# How do you calculate the tip amount on a resturant bill given the tip percentage?
+   # a) Figure out the formula and what the script would look like, making up example values as needed.
+  #b) Create the script
+
+# Script: 
+Bill = 75.89
+Tip_percent = .30 # 30%
+
+# Calculations
+Tip = Bill * Tip_percent
+
+# Results
+print(f"The tip on a ${Bill} restaurnant bill is ${Tip:.2f}")
+
+# Output:  The tip on a $75.89 resturant bill is $22.77

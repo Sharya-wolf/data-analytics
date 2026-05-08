62113f5b110b327e6f7c472ad9c365c47e19f047
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-08 13:00:44 -0400

    Completed Lab3

---
+++ week-05/Lab3_input_Ex2A.py
@@ -0,0 +1,30 @@
+# Sha'Rya Weaver
+# 5/7/2026
+
+# Lab 3
+# Script: Modified Net Worth using User Input
+
+
+# User input total assets and debts
+Assets = eval(input("Enter your total assests"))
+Debts = eval(input("Enter your total debts"))
+
+# Calculations
+Net_worth = Assets - Debts
+
+# Results
+print(f"Your total assests are ${Assets}")
+print(f"Your total debts are ${Debts}")
+print(f"Your net worth is {Net_worth}")
+
+# Output:
+  # Your total assets are $181.57
+  # Your debts are $2075.48
+  # Your net worth is -1893.91
+
+# Observations:
+ # As the program runs each line when it gets to the user input I noticed it tells you or stops in the terminal until you 
+ # put a number or your answer int the terminal. Once you press enter it continue to run into the next line taking your 
+ # input into consideration as it goes or stopping again when seeing another user input. I find it very interesting thinking
+ # of when and how we would use it. For example if we made a program and then pass it out so when it run the customer or co-
+ # worker can input their information in case I don't know it. Then it immidiately goes into printing the script.

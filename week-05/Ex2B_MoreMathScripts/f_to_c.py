489911981a0c008dc6a0c4f16c2de596102c16e2
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-11 09:21:23 -0400

    Completed f to c

---
+++ week-05/Ex2B_MoreMathScripts/f_to_c.py
@@ -0,0 +1,25 @@
+# 
+# Sha'Rya Weaver
+# 5/7/2026
+
+
+# 1. How do you convert a temperature from Fahrenheit to Celsius?
+
+#   To convert a Temperature from Celsius to Fahrenheit the formala is
+#   F = (9 / 5) * C + 32. 
+
+# Script
+Celsius = 75
+
+# Calculations
+Fahrenheit = (9 / 5) * Celsius + 32
+
+# Result
+print(f"It is {Celsius:.1f} celsuis degrees outside")
+print(f"The degrees for {Celsius:.1f} in fahrenheit is {Fahrenheit}")
+
+# Output
+ # It is 75.0 celsuis degrees outside
+ # The degrees for 75.0 in fahrenheit is 167.0
+
+

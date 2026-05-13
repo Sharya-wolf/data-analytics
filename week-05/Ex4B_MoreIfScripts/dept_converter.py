4ec2607d4aae19a63e3da8bf2c8bf3542deadc66
Sha'Rya <sharyaweaver3@gmail.com>
2026-05-13 16:00:48 -0400

    Completed dept converter

--- week-05/Ex4B_MoreIfScripts/dept_converter.py
+++ week-05/Ex4B_MoreIfScripts/dept_converter.py
@@ -15,10 +15,22 @@ dept_code = int(input("Please enter your departent code: "))
 
 if dept_code == 1:
     print("Marketing is your department!")
+elif dept_code == 5:
+    print("Human Resources is your department!")
+elif dept_code == 10:
+    print("Accounting is your department!")
+elif dept_code == 12:
+    print("Legal is your department!")
+elif dept_code == 18:
+    print("IT is your department!")
+elif dept_code == 20:
+    print("Customer Relations is your department!")
+else:
+    print("Code not found. Please try again.")
 
+# Output: Please enter your departent code: 13 Code not found. Please try again
 
-
-
+# Output: Please enter your departent code: 10 Accounting is your department!
 
 
 
